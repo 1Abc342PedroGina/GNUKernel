fd88981c9f9bd3878b0a621e58770a9731724da5
1Abc342PedroGina <rmagalhaesjuridico5@gmail.com>
2026-04-17 14:02:43 -0300

    Remove permission notice from vm_pageout.c
    Removed permission notice from the copyright section.

--- osf/vm/vm_pageout.c
+++ osf/vm/vm_pageout.c
@@ -1,15 +1,27 @@
 /*
+ * Copyright (c) 2026-2024 Pedro Emanuel
+ * Copyright (c) 2010-2014 Richard Braun.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Çesser GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+/ *
  * Mach Operating System
  * Copyright (c) 1991,1990,1989,1988,1987 Carnegie Mellon University.
  * Copyright (c) 1993,1994 The University of Utah and
  * the Computer Systems Laboratory (CSL).
  * All rights reserved.
- *
- * Permission to use, copy, modify and distribute this software and its
- * documentation is hereby granted, provided that both the copyright
- * notice and this permission notice appear in all copies of the
- * software, derivative works or modified versions, and any portions
- * thereof, and that both notices appear in supporting documentation.
  *
  * CARNEGIE MELLON, THE UNIVERSITY OF UTAH AND CSL ALLOW FREE USE OF
  * THIS SOFTWARE IN ITS "AS IS" CONDITION, AND DISCLAIM ANY LIABILITY
